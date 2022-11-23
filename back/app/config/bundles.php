98640fb55b6dbee1ff4506057d878aa66fa77649
LisaAu <au.lisa08@gmail.com>
2022-11-23 16:38:24 +0100

    add migration + endpoint login

--- back/app/config/bundles.php
+++ back/app/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
---
+++ back/app/src/Controller/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Firebase\JWT\JWT;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends AbstractController
+{
+
+    /**
+     * @Route (path="/login", name="app_login")
+     * @return JsonResponse
+     * @var $user ?User
+     */
+
+    public function login($appSecret) : JsonResponse {
+
+        $user = $this->getUser();
+
+        if($user === null){
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $jwt = JWT::encode([
+            'username' => $user->getUsername(),
+            'id' => $user->getId()
+        ],
+            $appSecret,
+            'HS256');
+
+        return $this->json([
+            'jwt' => $jwt
+        ]);
+
+    }
+
+    /**
+     * @Route (path="/user-list", name="app_user_list", methods="GET")
+     * @return JsonResponse
+     * @var $user ?User
+     */
+
+    public function userList(UserRepository $userRepository): JsonResponse
+    {
+
+    }
+
+}
